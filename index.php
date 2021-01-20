0adcb7299773c6d619ebc932af08ee7b5cecf79c
JoaoVictorLNacional <viana.joao@linknacional.com>
2021-01-20 16:13:56 -0300

    add page

--- index.php
+++ index.php
@@ -29,23 +29,20 @@ if (!class_exists('login_whmcs_shortcode')) {
     {
         public function __construct()
         {
-            add_shortcode('whmcslogin', [$this, 'login_whmcs_shortcode']);
             add_action('wp_enqueue_scripts', [$this, 'func_load_vuescripts']);
+            add_shortcode('whmcslogin', [$this, 'login_whmcs_shortcode']);
         }
 
         /// CRIAR O SHORTCODE
         public function login_whmcs_shortcode($atts = [], $content = null, $tag = '')
         {
-            define('ROOT_PATH', dirname(__FILE__));
-
-            $a = shortcode_atts([
-                'title' => '',
-                'hidefds' => '',
-                'class' => '',
-            ], $atts);
-
             wp_enqueue_script('wpvue_vuejs');
             wp_enqueue_script('wpvue_vuejs1');
+            wp_enqueue_script('wpvue_vuejs2');
+            wp_enqueue_script('wpvue_vuejs3');
+            wp_enqueue_script('wpvue_vuejs4');
+            wp_enqueue_style('wpvue_vuecss');
+            wp_enqueue_style('wpvue_vuecss1');
 
             return file_get_html(plugin_dir_url(__FILE__).'dist/spa/index.html');
         }
@@ -54,6 +51,11 @@ if (!class_exists('login_whmcs_shortcode')) {
         {
             wp_register_script('wpvue_vuejs', plugin_dir_url(__FILE__).'dist/spa/js/app.c54610b5.js', true);
             wp_register_script('wpvue_vuejs1', plugin_dir_url(__FILE__).'dist/spa/js/vendor.e947b7ef.js', true);
+            wp_register_script('wpvue_vuejs2', plugin_dir_url(__FILE__).'dist/spa/js/2.bf7586da.js', true);
+            wp_register_script('wpvue_vuejs3', plugin_dir_url(__FILE__).'dist/spa/js/3.757d20cb.js', true);
+            wp_register_script('wpvue_vuejs4', plugin_dir_url(__FILE__).'dist/spa/js/4.25d689bd.js', true);
+            wp_enqueue_style('wpvue_vuecss', plugin_dir_url(__FILE__).'dist/spa/css/app.0e433876.css', true);
+            wp_enqueue_style('wpvue_vuecss1', plugin_dir_url(__FILE__).'dist/spa/css/vendor.c27c6ce1.css', true);
         }
     }
     $matinalInit = new Wp_login_screen_whmcs();
