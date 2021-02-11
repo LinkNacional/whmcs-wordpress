2d10b7a3e30ada611e25e3424b1026eb12866e40
JoaoVictorLNacional <viana.joao@linknacional.com>
2021-02-11 10:42:24 -0300

    atualizado com a build de dev

--- index.php
+++ index.php
@@ -37,14 +37,14 @@ if (!class_exists('login_whmcs_shortcode')) {
         }
 
         public function func_load_vuescripts() {
-            wp_register_script('wpvue_vuejs', plugin_dir_url(__FILE__) . 'dist/spa/js/app.e6ae34a1.js', true);
-            wp_register_script('wpvue_vuejs1', plugin_dir_url(__FILE__) . 'dist/spa/js/vendor.891ac004.js', true);
+            wp_register_script('wpvue_vuejs', plugin_dir_url(__FILE__) . 'dist/spa/js/app.768f316d.js', true);
+            wp_register_script('wpvue_vuejs1', plugin_dir_url(__FILE__) . 'dist/spa/js/vendor.e535d9b7.js', true);
             wp_register_script('wpvue_vuejs2', plugin_dir_url(__FILE__) . 'dist/spa/js/2.dcb47d05.js', true);
             wp_register_script('wpvue_vuejs3', plugin_dir_url(__FILE__) . 'dist/spa/js/3.757d20cb.js', true);
-            wp_register_script('wpvue_vuejs4', plugin_dir_url(__FILE__) . 'dist/spa/js/4.d5f4739e.js', true);
+            wp_register_script('wpvue_vuejs4', plugin_dir_url(__FILE__) . 'dist/spa/js/4.68c35e80.js', true);
 
             wp_enqueue_style('wpvue_vuecss', plugin_dir_url(__FILE__) . 'dist/spa/css/app.0e433876.css', true);
-            wp_enqueue_style('wpvue_vuecss1', plugin_dir_url(__FILE__) . 'dist/spa/css/vendor.c27c6ce1.css', true);
+            wp_enqueue_style('wpvue_vuecss1', plugin_dir_url(__FILE__) . 'dist/spa/css/vendor.e810699e.css', true);
         }
     }
     $matinalInit = new Wp_login_screen_whmcs();
