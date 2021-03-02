8536df3a0b7f60f928e0cde03a55cadae45b5902
JoaoVictorLNacional <viana.joao@linknacional.com>
2021-03-02 14:09:20 -0300

    removido projeto quasar

--- quasar/src/boot/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-var url = login_whmcs_url
-
-const axiosInstance = axios.create({
-    baseURL: url + '/wp-json/login-whmcs/v1/login/',
-    timeout: 90000
-  })
-export { axiosInstance }
--- quasar/src/boot/vars.js
+++
@@ -1,3 +0,0 @@
-var textHeader = login_whmcs_content
-var divSize = login_whmcs_size
-export { textHeader, divSize }
